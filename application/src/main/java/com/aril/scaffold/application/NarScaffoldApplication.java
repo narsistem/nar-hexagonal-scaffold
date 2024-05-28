adc757a0099d8ddc2a61f885c14c80c11a5e2772
SenolAtac <senol.atac@nar.com.tr>
2024-05-28 17:54:02 +0300

    added swagger

--- application/src/main/java/com/aril/scaffold/application/NarScaffoldApplication.java
+++ application/src/main/java/com/aril/scaffold/application/NarScaffoldApplication.java
@@ -1,10 +1,16 @@
 package com.aril.scaffold.application;
 
 import com.aril.scaffold.infra.InfraAppConfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "NAR-HEXA-SCAFFOLD MODULE API DOC",
+                version = "1.0"))
 @SpringBootApplication
 @Import(InfraAppConfig.class)
 public class NarScaffoldApplication {
